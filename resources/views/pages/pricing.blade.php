fd47c8d890d8f15587f76b09842185d5e1d92023
Authentic48 <iboufall48@gmail.com>
2021-01-08 20:55:01 +0700

    pricing updated

--- resources/views/pages/pricing.blade.php
+++ resources/views/pages/pricing.blade.php
@@ -24,4 +24,86 @@
     </div>
 </div>
 <!-- slider-end -->
+
+ <!-- courses start -->
+ <div class="courses-area courses-bg-height gray-bg pt-100 pb-70">
+    <div class="container">
+        <div class="courses-list">
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 col-md-6">
+                    <div class="courses-wrapper mb-30">
+                        <div class="courses-content courses-content-2 text-center">
+                            <div class="courses-thumb">
+                                <img src="{{ asset('assets/img/courses/coursesthumb001.jpg')}}" alt="">
+                            </div>
+                            <div class="courses-heading text-center">
+                                <h1>Занятия в группах</h1>
+                            </div>
+                            <div class="courses-icon text-center">
+                                <div class="courses-single-icon courses-single-icon-2">
+                                    <span class="price">Цена</span>
+                                    <span class="user-number">1000/Ч.</span>
+                                </div>
+                                <div class="courses-single-icon courses-single-icon-2">
+                                    <span class="ti-user"></span>
+                                    <span class="seat">Студенты</span>
+                                    <span class="user-number">4</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-lg-4 col-md-6">
+                    <div class="courses-wrapper mb-30">
+                        <div class="courses-content courses-content-2 text-center">
+                            <div class="courses-thumb">
+                            <img src="{{ asset('assets/img/courses/coursesthumb002.jpg')}}" alt="">
+                            </div>
+                            <div class="courses-heading text-center">
+                                <h1>Подготовка к IELTS/TOEFL</h1>
+                            </div>
+                            <div class="courses-icon text-center">
+                                <div class="courses-single-icon courses-single-icon-2">
+                                    <span class="price">Цена</span>
+                                    <span class="user-number">1000/Ч.</span>
+                                </div>
+                                <div class="courses-single-icon courses-single-icon-2">
+                                    <span class="ti-user"></span>
+                                    <span class="seat">Студент</span>
+                                    <span class="user-number">1</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-lg-4 col-md-6">
+                    <div class="courses-wrapper mb-30">
+                        <div class="courses-content courses-content-2 text-center">
+                            <div class="courses-thumb">
+                            <img src="{{ asset('assets/img/courses/coursesthumb003.jpg')}}" alt="">
+                            </div>
+                            <div class="courses-heading text-center">
+                                <h1>Индивидуальное обучение</h1>
+                            </div>
+                            <div class="courses-icon text-center">
+                                <div class="courses-single-icon courses-single-icon-2">
+                                    <span class="price">Цена</span>
+                                    <span class="user-number">700/Ч.</span>
+                                </div>
+                                <div class="courses-single-icon courses-single-icon-2">
+                                    <span class="ti-user"></span>
+                                    <span class="seat">Студенты</span>
+                                    <span class="user-number">4</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+ </div>
 @endsection
